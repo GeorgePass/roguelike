a49a9379030ebabc8b15894f0cd838eed8c35ea9
GeorgePass <georgepassos2@gmail.com>
2023-05-09 21:22:07 +0300

    new interface update

--- engine.py
+++ engine.py
@@ -1,34 +1,52 @@
-from typing import Set, Iterable, Any
+from __future__ import annotations
 
-from tcod.context import Context
+from typing import TYPE_CHECKING
+from render_functions import render_bar, render_names_at_mouse_location
 from tcod.console import Console
-from game_map import GameMap
-from entity import Entity
-from input_handlers import EventHandler
+from tcod.map import compute_fov
+
+from input_handlers import MainGameEventHandler
+from message_log import MessageLog
+
+if TYPE_CHECKING:
+    from entity import Actor
+    from game_map import GameMap
+    from input_handlers import EventHandler
 
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
-        self.event_handler = event_handler
-        self.game_map = game_map
+    game_map: GameMap
+
+    def __init__(self, player: Actor):
+        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
+        self.message_log = MessageLog()
+        self.mouse_location = (0, 0)
 
-    def handle_events(self, events: Iterable[Any]) -> None:
-        for event in events:
-            action = self.event_handler.dispatch(event)
+    def handle_enemy_turns(self) -> None:
+        for entity in set(self.game_map.actors) - {self.player}:
+            if entity.ai:
+                entity.ai.perform()
 
-            if action is None:
-                continue
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
 
-            action.perform(self, self.player)
-
-    def render(self, console: Console, context: Context) -> None:
+    def render(self, console: Console) -> None:
         self.game_map.render(console)
 
-        for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+        self.message_log.render(console=console, x=21, y=45, width=40, height=5)
 
-        context.present(console)
-
-        console.clear()
+        render_bar(
+            console = console,
+            current_value = self.player.fighter.hp,
+            maximum_value = self.player.fighter.max_hp,
+            total_width=20,
+              )
+        render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
